5cee41ecaa26e5debe545b323d7320a8d946de48
CosimoA <cosimo.archip@gmail.com>
2023-12-13 15:32:45 +0100

    CONFIG: setup base scaffolding

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/*
+Descrizione:
+Attraverso l’apposita API di Boolean:
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+*/
+
+import { createApp } from 'vue'
+
+const app = createApp({
+    data() {
+        
+    },
+    methods: {
+        
+    }
+}).mount('#app')
